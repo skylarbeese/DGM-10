3f8745016dc9e4c83e17993f735e3e4a500fab90
SKYLAR BEESE <skylarbeese@gmail.com>
2021-03-26 20:45:11 -0600

    bc

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ async function getData() {
 let GetHotelData = {}
 getData().then(data => GetHotelData = data)
 
-document.querySelectorAll('#mer').addEventListener('click', info)
+document.querySelector('#mer').addEventListener('click', info)
 
 function info(e) {
 
